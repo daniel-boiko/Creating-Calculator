8a6dcb1b3a047948f94fdf8af4b8816b25553059
Paul Susanin <pasha.susanin15521552@mail.ru>
2021-08-06 13:39:55 +0300

    fix: fixed input structure

--- Base.php
+++ Base.php
@@ -1,9 +1,9 @@
 <?php
 
-if($_POST['sibmit'])
+if($_POST['submit'])
 {
     $a = (int)$_POST['first'];
-    $b = (int)$_POST['operation'];
+    $b = (int)$_POST['second'];
     $p = $_POST['operation'];
     $c;
     if ($p == "+")
@@ -22,41 +22,29 @@ if($_POST['sibmit'])
 
 ?>
 
-
 <form action = "" method = "post">
-
+    First number
     <input type = "text" name = "first" required>
-
+    <br>
+    Operator
     <select name = "operation" required>
         <option value = "+">+</option>
         <option value = "-">-</option>
         <option value = "*">*</option>
         <option value = "/">/</option>
     </select>
-
+    <br>
+    Second number
     <input type = "text" name = "second" required>
-
+    <br>
+    Try it out
     <input type = "submit" name  = "submit">
-
+    <br>
+    Answer:
 </form>
 
 <?php
 if($c)
     echo $c;
 ?>
-
-
-// here output
-echo("summa: " . $sum);
-echo ("<br");
-echo("Subtraction: " . $min);
-echo ("<br");
-echo("Multiplying: " . $multi);
-echo ("<br");
-echo("Dividing: " . $del);
-echo ("<br");
-//
-?>
-</body>
-</html>
 
