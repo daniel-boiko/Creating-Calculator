8167596eb196a2bd9e1be3b267e9d04ce629cc4b
strxms <boyko.d.a@yandex.ru>
2021-08-06 10:47:06 +0300

    feat: creating math operations (#3)

--- Base.php
+++ Base.php
@@ -5,9 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Calculator</title>
 </head>
 <body>
+<?php
+// here input
 
+//
+
+$sum=$a+$b;
+$min=$a-$b;
+$multi=$a*$b;
+$del=$a/$b;
+
+// here output
+
+//
+?>
 </body>
 </html>
