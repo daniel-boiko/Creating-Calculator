a78ff6ccf0c98c0f02b1bbb0ece40722bfa821a7
strxms <boyko.d.a@yandex.ru>
2021-08-06 13:39:44 +0300

    fix: delete warnings, weak warnings (3)

--- Base.php
+++ Base.php
@@ -1,62 +1,60 @@
-<?php
-
-if($_POST['sibmit'])
-{
-    $a = (int)$_POST['first'];
-    $b = (int)$_POST['operation'];
-    $p = $_POST['operation'];
-    $c;
-    if ($p == "+")
-        $c = $a + $b;
-    else if($p == "-")
-        $c = $a - $b;
-    else if($p == "*")
-        $c = $a * $b;
-    else if ($p = "/")
-    {
-        if($b == 0)
-            echo "Cannot divide by 0";
-        else $c = $a / $b;
-    }
-}
-
-?>
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculator</title>
+</head>
+<body>
 
 <form action = "" method = "post">
 
-    <input type = "text" name = "first" required>
+    <label>
+        <input type = "text" name = "first" required>
+    </label>
 
-    <select name = "operation" required>
+    <label>
+        <select name = "operation" required>
         <option value = "+">+</option>
         <option value = "-">-</option>
         <option value = "*">*</option>
         <option value = "/">/</option>
-    </select>
+        </select>
+    </label>
 
+    <label>
     <input type = "text" name = "second" required>
-
     <input type = "submit" name  = "submit">
+    </label>
 
+    <?php
+
+    if($_POST['submit'])
+    {
+        $a = (int)$_POST['first'];
+        $b = (int)$_POST['second'];
+        $p = $_POST['operation'];
+        $c=0;
+        if ($p == "+")
+            $c = $a + $b;
+        else if($p == "-")
+            $c = $a - $b;
+        else if($p == "*")
+            $c = $a * $b;
+        else if ($p = "/")
+        {
+            if($b == 0)
+                echo "Cannot divide by 0";
+            else $c = $a / $b;
+        }
+
+        echo "<br> Answer: $c";
+    }
+
+    ?>
 </form>
-
-<?php
-if($c)
-    echo $c;
-?>
-
-
-// here output
-echo("summa: " . $sum);
-echo ("<br");
-echo("Subtraction: " . $min);
-echo ("<br");
-echo("Multiplying: " . $multi);
-echo ("<br");
-echo("Dividing: " . $del);
-echo ("<br");
-//
-?>
 </body>
 </html>
 
